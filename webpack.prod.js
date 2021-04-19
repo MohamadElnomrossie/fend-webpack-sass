21d3bc3ed793b28bbb85d58ff6be022810b8546b
Mohamad Elnomrossie <m.elnomrossie@gmail.com>
2021-04-19 11:18:57 +0200

    minigyinh

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,23 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const common=require('./webpack.common')
 const {merge}=require('webpack-merge')
+const MiniCssExtractPlugin=require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = merge(common,{
     
  mode: 'production',
-  
+ optimization:{
+     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+ },
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader","sass-loader"]
+            },
+        ],
+    },
+        plugins:[new MiniCssExtractPlugin({filename:'[name].css'})]
+
 })
