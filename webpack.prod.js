57da832b89cddc6d6440826c3a6a04c62cdd3c2f
Mohamad Elnomrossie <m.elnomrossie@gmail.com>
2021-04-19 10:32:21 +0200

    fixing-js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+const common=require('./webpack.common')
+const {merge}=require('webpack-merge')
+module.exports = merge(common,{
+    
+ mode: 'production',
+  
+})
